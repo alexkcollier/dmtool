9e493cd5eee0adc04f4f1921fae4097103aaa15d
Alex Collier <alexkcollier@gmail.com>
2017-12-09 20:01:57 -0500

    Improve styling and move transition to default.vue

--- pages/magic-items.vue
+++ pages/magic-items.vue
@@ -9,99 +9,105 @@
       </h2>
 
       <div class="content">
-        <div class="columns">
-          <div class="column">
 
-            <b-field grouped>
-              <div class="control is-expanded has-icons-left">
-                <input 
-                  :class="{'is-danger': !filteredItems.length}"
-                  @keyup="makeQuery"
-                  v-model="search" 
-                  class="input"
-                  type="text"
-                  placeholder="Search for items">
-                  <b-icon icon="magnify" size="is-small" class="is-left"></b-icon>
-              </div>
-              <!-- TODO: add rarity filter -->
-              <b-switch>Hi</b-switch>
-            </b-field>
+        <b-field grouped>
+          <div class="control is-expanded has-icons-left">
+            <input 
+              :class="{'is-danger': !filteredItems.length}"
+              @keyup="makeQuery"
+              v-model="search" 
+              class="input"
+              type="text"
+              placeholder="Search for items">
+              <b-icon icon="magnify" size="is-small" class="is-left"></b-icon>
+          </div>
+          <!-- TODO: add rarity filter -->
+          <!-- <b-switch>Hi</b-switch> -->
+        </b-field>
+        <hr>
 
-            <!-- Loop through items -->
-            <div v-for="item in filteredItems" :key="item.index">
-              <!-- Only display one item at a time -->
-              <h3 @click="showItem(item.name)">{{ item.name }}</h3>
-              <transition name="fade-grow">
-                <div v-if="activeItem === item.name">
+        <!-- Loop through items -->
+        <div v-for="item in filteredItems" :key="item.index">
+          
+          <!-- Item name and rarity -->
+          <!-- Only display one item at a time -->
+          <div @click="showItem(item.name)" class="">
+            <a>
+              <h3 class="title">{{ item.name }}</h3>
 
-                  <!-- Item types, rarity, and attunement -->
-                  <h4>
-                    <span v-if="item.wondrous">Wondrous item</span>
-                    <span v-else>{{ item.type }}</span>
-                    <span>, {{ item.rarity }}</span>
+              <!-- Rarity and attunement -->
+              <h6 class="subtitle is-item-rarity is-size-6 is-italic">
+                <span v-if="item.wondrous">Wondrous item</span>
+                <span v-else>{{ item.type }}</span>
+                <span>, {{ item.rarity }}</span>
 
-                    <!-- Attunement options -->
-                    <span v-if="item.reqAttune === 'YES'"> (requires attunement)</span>
-                    <span v-else-if="item.reqAttune"> (requires attunement {{ item.reqAttune }})</span>
-                  </h4>
+                <!-- Attunement options -->
+                <span v-if="item.reqAttune === 'YES'"> (requires attunement)</span>
+                <span v-else-if="item.reqAttune"> (requires attunement {{ item.reqAttune }})</span>
+              </h6>
+            </a>
+          </div>
 
-                  <!-- Iterate item entries -->
-                  <!-- TODO: cleaner implementation -->
-                  <template v-for="entry in item.entries" >
+          <!-- Item description -->
+          <transition name="fade-grow" mode="out-in">
+            <div v-if="activeItem === item.name" :key="item.name">
 
-                    <!-- Paragraphs -->
-                    <p v-if="!entry.type" :key="entry.index">
-                      {{ entry }}
-                    </p>
+              <!-- Iterate item entries -->
+              <!-- TODO: cleaner implementation -->
+              <template v-for="entry in item.entries" >
+
+                <!-- Paragraphs -->
+                <p v-if="!entry.type" :key="entry.index">
+                  {{ entry }}
+                </p>
+
+                <!-- Lists -->
+                <ul v-else-if="entry.type =='list'" :key="entry.index">
+                  <li v-for="item in entry.items" :key="item.index">{{ item }}</li>
+                </ul>
+
+                <!-- Tables -->
+                <table v-else-if="entry.type == 'table'" :key="entry.index">
+                  <thead>
+                    <tr>
+                      <th v-for="label in entry.colLabels" :key="label.index">{{ label }}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="row in entry.rows" :key="row.index">
+                      <td v-for="cell in row" :key="cell.index">{{ cell }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <!-- Entries -->
+                <template v-else-if="entry.type =='entries'">
+                  <h5 :key="entry.name">{{ entry.name }}</h5>
+                  <template v-for="entry in entry.entries">
 
                     <!-- Lists -->
-                    <ul v-else-if="entry.type =='list'" :key="entry.index">
+                    <ul v-if="entry.type =='list'" :key="entry.index">
                       <li v-for="item in entry.items" :key="item.index">{{ item }}</li>
                     </ul>
 
-                    <!-- Tables -->
-                    <table v-else-if="entry.type == 'table'" :key="entry.index">
-                      <thead>
-                        <tr>
-                          <th v-for="label in entry.colLabels" :key="label.index">{{ label }}</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="row in entry.rows" :key="row.index">
-                          <td v-for="cell in row" :key="cell.index">{{ cell }}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-
-                    <!-- Entries -->
-                    <template v-else-if="entry.type =='entries'">
-                      <h5 :key="entry.name">{{ entry.name }}</h5>
-                      <template v-for="entry in entry.entries">
-
-                        <!-- Lists -->
-                        <ul v-if="entry.type =='list'" :key="entry.index">
-                          <li v-for="item in entry.items" :key="item.index">{{ item }}</li>
-                        </ul>
-
-                        <!-- Paragraphs -->
-                        <p v-else :key="entry.index">
-                          {{ entry }}
-                        </p>
-
-                      </template>
-                    </template>
-
-                    <!-- Highlight entries of unexpected type. -->
-                    <p v-else :key="entry.index" style="color: red;">{{ entry }}</p>
+                    <!-- Paragraphs -->
+                    <p v-else :key="entry.index">
+                      {{ entry }}
+                    </p>
 
                   </template>
-                  <p class="is-italic">{{ item.source }}, page {{ item.page }}</p>
-                  <hr>
-                </div>
-              </transition>
+                </template>
+
+                <!-- Highlight entries of unexpected type. -->
+                <p v-else :key="entry.index" style="color: red;">{{ entry }}</p>
+
+              </template>
+              <p class="is-italic">{{ item.source }}, page {{ item.page }}</p>
             </div>
-          </div>
+          </transition>
+          <hr>
         </div>
+
       </div>
     </div>
   </section>
@@ -130,8 +136,8 @@ export default {
     filteredItems () {
       return lodash.filter(this.orderedItems, (item) => {
         // TODO: Include rarity filter
-        return lodash.includes(lodash.toLower(item.name), lodash.toLower(this.query))
-        // && (item.rarity === 'Rare') // This sets rarity
+        return lodash.includes(lodash.toLower(item.name), lodash.toLower(this.query)) &&
+        (item.rarity !== 'None') // This sets rarity
       })
     }
   },
@@ -151,21 +157,8 @@ export default {
 </script>
 
 <style lang="scss">
-$fade-time: 500ms;
-
-.fade-grow {
-  &-enter-active, &-leave-active {
-    transition: all $fade-time ease-in-out;
-    max-height: 300vh;
-    overflow: hidden;
-  }
-  &-enter, &-leave-to {
-    max-height: 0;
-    opacity: 0;
-  }
-}
-.height-0 { height: 0; }
-.items h2, .items h3 {
-  cursor: pointer;
+.is-item-rarity {
+  padding-top:0.3em;
+  // margin-bottom: 0!important;
 }
 </style>
