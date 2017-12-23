3cf59785c3c045297785004fe2768c31343ec2a7
Alex Collier <alexkcollier@gmail.com>
2017-12-23 18:09:32 -0500

    Remove data from home

--- pages/index.vue
+++ pages/index.vue
@@ -15,74 +15,12 @@
 </template>
 
 <script>
-import lodash from 'lodash'
-import items from '~/data/items.json'
-
 export default {
   head () {
     return { title: 'Home' }
-  },
-  data () {
-    return {
-      items,
-      activeRarity: null,
-      activeItem: null,
-      rarities: {
-        // none: 'None',
-        common: 'Common',
-        uncommon: 'Uncommon',
-        rare: 'Rare',
-        veryRare: 'Very Rare',
-        legendary: 'Legendary',
-        artifact: 'Artifact'
-      }
-    }
-  },
-  computed: {
-    orderedItems () {
-      return lodash.orderBy(this.items.item, 'name')
-    }
-  },
-  methods: {
-    showRarity (arg) {
-      // Only display one rarity at a time and collapse items when changing rarity
-      if (this.activeRarity === arg) {
-        this.activeRarity = null
-      } else {
-        this.activeRarity = arg
-      }
-      this.activeItem = null
-      return [this.activeRarity, this.activeItem]
-    },
-    showItem (arg) {
-      // Only display one item at a time
-      if (this.activeItem === arg) {
-        this.activeItem = null
-      } else {
-        this.activeItem = arg
-      }
-      return this.activeItem
-    }
   }
 }
 </script>
 
 <style lang="scss">
-$fade-time: 500ms;
-
-.fade-grow {
-  &-enter-active, &-leave-active {
-    transition: all $fade-time ease-in-out;
-    max-height: 300vh;
-    overflow: hidden;
-  }
-  &-enter, &-leave-to {
-    max-height: 0;
-    opacity: 0;
-  }
-}
-
-.items h2, .items h3 {
-  cursor: pointer;
-}
 </style>
