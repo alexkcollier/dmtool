faf411a9239039d4b9cfab0d8677423a99330e43
Alex Collier <alexkcollier@gmail.com>
2017-12-23 19:18:30 -0500

    Move transitions to scss

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,5 @@
 </template>
 
 <style lang="scss">
-$fade-time: 500ms;
 
-.fade-grow {
-  &-enter-active, &-leave-active {
-    transition: all $fade-time ease-in-out;
-    max-height: 300vh;
-    overflow: hidden;
-  }
-  &-enter, &-leave-to {
-    max-height: 0;
-    opacity: 0;
-  }
-}
 </style>
