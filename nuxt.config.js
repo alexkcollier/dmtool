40c16f71599df4cede502e0952e9c27722c814c9
Alex Collier <alexkcollier@gmail.com>
2017-12-23 22:42:24 -0500

    add theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Personal DM tools' }
+      { hid: 'description', name: 'description', content: 'Personal DM tools' },
+      { name: 'theme-color', content: '#3d4f5d'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
