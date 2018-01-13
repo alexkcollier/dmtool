623353580ffa1f62282c255a1659251ee04ea1d1
Alex Collier <alexkcollier@gmail.com>
2018-01-13 12:32:27 -0500

    Scaffold full stat block

--- pages/bestiary.vue
+++ pages/bestiary.vue
@@ -4,13 +4,13 @@
       <h1 class="title">
         <nuxt-link to="/">dmtool</nuxt-link>
       </h1>
-      <!-- {{ $router.options.routes }} -->
       <div class="content">
         <h1>Bestiary</h1>
 
         <h3 class="title">{{ bestiary.monster[0].name }}</h3>
         <h6>
-          <!-- currently does not work if type is not object literal -->
+          <!-- TODO: currently does not work if type is not object literal -->
+          <!-- TODO: remove trailing comma from tags -->
           {{ bestiary.monster[0].size }} {{ bestiary.monster[0].type.type }}<template v-if="bestiary.monster[0].type.tags"> (<span v-for="tag in bestiary.monster[0].type.tags" :key="tag">{{ tag }}, </span>)</template>,
           {{ bestiary.monster[0].alignment }}
         </h6>
@@ -49,6 +49,9 @@
         <!-- Other stats -->
         <div>
           <p v-if="bestiary.monster[0].save">Saving Throws: {{ bestiary.monster[0].save }}</p>
+          
+          <!-- TODO: Parse skills object -->
+          <!-- TODO: Use bold instead of colon -->
           <p v-if="bestiary.monster[0].skill">Skills: {{ bestiary.monster[0].skill }}</p>
           <p v-if="bestiary.monster[0].resist">Damage Resistances: {{ bestiary.monster[0].resist }}</p>
           <p v-if="bestiary.monster[0].immune">Damage Immunities: {{ bestiary.monster[0].immune }}</p>
@@ -59,14 +62,28 @@
           <p>Challenge: {{ bestiary.monster[0].cr }}</p>
         </div>
         
+        <!-- Traits -->
+        <div v-if="bestiary.monster[0].trait">
+          <template v-for="trait in bestiary.monster[0].trait">
+            <p :key="trait.name"><strong>{{ trait.name }}.</strong> {{ trait.text[0] }}</p>
 
-        <!-- Traits need some crazy stuff to work -->
-        <template v-if="bestiary.monster[0].trait">
-          <p v-for="trait in bestiary.monster[0].trait" :key="trait.name">
-            {{ trait.name }}. {{ trait.text[0] }}
-            <!-- <p v-for="p in trait.text" :key="p.index"> {{p}} </p> -->
-          </p>
-        </template>
+            <!-- TODO: Currently does not respect spellcasting (innate or learned) or multi-paragraph traits. -->
+            <p v-for="p in removeFirst(trait.text)" :key="p.index">{{ p }}</p>
+          </template>
+        </div>
+
+        <!-- Actions -->
+        <div v-if="bestiary.monster[0].action">
+          <h2>Actions</h2>
+          <template v-for="action in bestiary.monster[0].action">
+
+            <!-- TODO: Attack formatting -->
+            <p :key="action.name"><strong>{{ action.name }}.</strong> {{ action.text[0] }}</p>
+
+            <!-- TODO: Currently does not respect spellcasting (innate or learned) or multi-paragraph actions. -->
+            <p v-for="p in removeFirst(action.text)" :key="p.index">{{ p }}</p>
+          </template>
+        </div>
 
         <!-- <div v-for="legendary in bestiary.legendaryGroup" :key="legendary.name">
           {{ legendary.name }}
@@ -88,11 +105,16 @@ export default {
       bestiary
     }
   },
-  computed: {
-    bestiaryKeys () {
-      // return Array.isArray(bestiary)
-      let k = Object.keys(this.bestiary)
-      return k
+  methods: {
+    removeFirst (arr) {
+      let r = []
+      // arr.splice(0, 1) and arr.shift() were creating infinite loops
+      if (arr.length > 1) {
+        for (let i in arr) {
+          r.push(arr[i])
+        }
+      }
+      return r.length > 0 ? r : null
     }
   }
 }
