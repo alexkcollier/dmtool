286c3532bcb8f1ecc1c8e1c8bac57c3d23510eef
Alex Collier <alexkcollier@gmail.com>
2018-02-07 13:10:56 -0500

    Move filtering to search component

--- components/Search/index.vue
+++ components/Search/index.vue
@@ -5,48 +5,64 @@
       <div class="control has-icons-left is-expanded">
         <input
           @keyup="query"
-          v-model="search" 
-          :class="{'is-danger': !filtered.length }"
+          v-model="searchTerm" 
+          :class="{'is-danger': !queryResult.length }"
           class="input"
           type="text"
           :placeholder="placeholder">
           <b-icon icon="magnify" size="is-small" class="is-left"></b-icon>
       </div>
       <div class="control">
-        <button class="button is-primary" style="margin:0;" :disabled="!search" @click="clearSearch">Clear</button>
+        <button class="button is-primary" style="margin:0;" :disabled="!searchTerm" @click="clearSearch">Clear</button>
       </div>
     </b-field>
 
+    <!-- Filters -->
     <div v-if="filterFields.length" class="card">
-      <!-- Filters -->
       <div class="card-header">
+
+        <!-- Filter collapse control-->
         <a @click="collapseFilters = !collapseFilters" class="card-header-title">
           Filters
           <b-icon :icon="collapseFilters ? 'chevron-down' : 'chevron-up'"/>
         </a>
       </div>
 
+      <!-- Filter options display -->
       <transition name="fade-grow">
         <div v-show="!collapseFilters">
-
+          
+          <!-- Filter options select -->
           <div class="card-header">
             <template v-for="(data, filter) in filters">
-              <a class="card-footer-item" :key="filter" :class="{'is-active': visibleFilterOptions === filter}" @click="showFilter = filter">{{ filter }}</a>
+              <a class="card-footer-item is-capitalized"
+                 :key="filter"
+                 :class="{'is-active': visibleFilterOptions === filter}"
+                 @click="visibleFilter = filter">
+                 {{ filter }}
+              </a>
             </template>
           </div>
 
+          <!-- Filter options -->
           <div class="card-content">
-            <template>
-              <b-field grouped group-multiline>
-                <div v-for="option in filters[visibleFilterOptions]" class="control" :key="option.name">
-                  <b-switch v-model="option.value">{{ option.name }}</b-switch>
-                </div>
-              </b-field>
-            </template>
+            <b-field grouped group-multiline>
+              <div v-for="option in filters[visibleFilterOptions]" class="control" :key="option.name">
+                <b-switch v-model="option.value" @input="query">{{ option.name }}</b-switch>
+              </div>
+            </b-field>
           </div>
+
         </div>
       </transition>
     </div>
+
+    <!-- Result count -->
+    <div class="control">
+      <div class="help has-text-right" :class="{'is-danger': resultCount == 0}">{{ resultCount }} item<span v-if="resultCount != 1">s</span> found.</div>
+    </div>
+
+    <hr>
   </div>
 </template>
 
@@ -66,11 +82,11 @@ export default {
   },
   data () {
     return {
-      search: '',
-      filtered: this.model,
+      searchTerm: '',
+      queryResult: _.sortBy(this.model, 'name'),
       collapseFilters: true,
       filters: {},
-      showFilter: ''
+      visibleFilter: ''
     }
   },
   computed: {
@@ -78,35 +94,70 @@ export default {
       return this.searchType ? `Search for ${this.searchType}` : 'Search'
     },
     visibleFilterOptions () {
-      return this.showFilter ? this.showFilter : (Object.keys(this.filters) ? Object.keys(this.filters)[0] : '')
-    }
+      return this.visibleFilter ? this.visibleFilter : (Object.keys(this.filters) ? Object.keys(this.filters)[0] : '')
+    },
+    resultCount: function () { return this.queryResult.length }
   },
   methods: {
     clearSearch: function () {
-      this.search = ''
+      this.searchTerm = ''
       this.query()
     },
-    getFilters: function (...filterList) { // Generate filter lists from data
-      filterList.forEach(filterList => {
-        let options = [...new Set(this.model.map(item => item[filterList]))]
-        options = options.map(item => { return { name: item, value: true } })
-        this.$set(this.filters, filterList, options) // Must use vm.set to make these properties reactive
+    getFilters: function (...filters) { // Generate filter lists from data
+      filters.forEach(filters => {
+        let options = [...new Set(this.model.map(item => item[filters]))] // Get all possible options from model
+        options = options.map(item => { return { name: item, value: true } }) // Create array of options
+        this.$set(this.filters, filters, options) // Add filters. Must use vm.set to make these properties reactive
       })
     },
     query: _.debounce(function () {
-      this.filtered = this.model.filter(i => {
-        return i[this.searchField].toLowerCase().includes(this.search.toLowerCase())
+      const filterTest = (filter, testValue) => { // check if filter includes testValue from model
+        let trueOptions = []
+        this.filters[filter].forEach(option => { if (option.value) trueOptions.push(option.name) }) // Create list of options that are true
+        return trueOptions.includes(testValue) // return if testValue is a true option
+      }
+
+      let result = this.model.filter(el => {
+        let filterTestRes = true // All filter options default to true
+        Object.keys(this.filters).forEach(filter => { filterTestRes *= filterTest(filter, el[filter]) }) // check each element against all filters
+        let test = el[this.searchField].toLowerCase().includes(this.searchTerm.toLowerCase()) &&
+        filterTestRes // ensure element contains search term and passes filter test
+        return test
       })
-      this.$emit('update-data', this.filtered)
+
+      this.queryResult = result
+
+      this.$emit('update-data', this.queryResult)
     }, 500)
   },
   created () {
     this.getFilters(...this.filterFields)
-    this.$emit('update-data', this.filtered)
+    this.$emit('update-data', this.queryResult)
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.card {
+  border-radius: 2px;
+  box-shadow: none;
+  &-header {
+    .is-active {
+      font-weight: 600;
+    }
+    &-title, {
+      padding: 0.36em 0.75em;
+    }
+  }
 
+  &-footer-item {
+    padding: 0.75em;
+  }
+
+  &-content {
+    padding-bottom: 1.25em;
+  }
+}
+
+hr { margin-top: 0.25em; }
 </style>
