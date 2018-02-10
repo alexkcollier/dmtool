0fac35f3e6e49e478b52438ec362bade69c6102d
Alex Collier <alexkcollier@gmail.com>
2018-02-10 10:56:17 -0500

    Format attacks

--- components/CreatureEntries/Trait.vue
+++ components/CreatureEntries/Trait.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <p><strong><i>{{ model.name }}.</i></strong> {{ model.text[0] }}</p>
+    <p>
+      <strong><i>{{ model.name }}. </i></strong>
+      <span v-html="attack(model.text[0])"></span>
+    </p>
     
     <!-- TODO: Currently does not respect spellcasting (innate or learned) or multi-paragraph models properly. -->
     <p v-for="p in shiftModel" :key="p.index">{{ p }}</p>
@@ -17,6 +20,14 @@ export default {
     shiftModel () {
       let r = this.model.text.slice(1)
       return r.length > 0 ? r : null
+    }
+  },
+  methods: {
+    attack: function (str) {
+      let attackRegExp = /(Melee|Ranged).*Attack:/g
+      let attackType = str.match(attackRegExp)
+      let result = str.replace(/(Melee|Ranged).*Attack:/g, `<i>${attackType}</i>`).replace(/Hit:/, '<i>Hit:</i>')
+      return result
     }
   }
 }
