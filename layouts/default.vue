3ae3219556020ac3cb52394e8489ffff97b5d1ed
Alex Collier <alexkcollier@gmail.com>
2018-02-27 10:32:56 -0500

    Improve navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,67 @@
 <template>
   <div>
-
-    <div class="container" style="margin-bottom: -3rem;">
-      <div @click="toggleNavigation" class="button is-text has-text-primary" style="margin-left:-0.5rem;">
-        <b-icon icon="menu"/>
+    <div class="page-header">
+      <div class="section">
+        <div class="container">
+          <div @click="toggleNavigation" class="is-menu-button has-text-primary">
+            <b-icon icon="menu"/>
+          </div>
+          <h1 class="title is-size-2">dmtool</h1>
+        </div>
       </div>
-    </div>
 
-    <!-- navigation -->
-    <transition name="slide">
-      <div v-show="showNavigation" class="hero is-fullheight is-primary is-bold is-navigation">
-        <div class="hero-head">
-          <div class="container">
-            <div @click="toggleNavigation" class="button is-text has-text-white ">
-              <b-icon icon="backburger"/> 
-              <span>Close navigation</span>
+      <!-- navigation -->
+      <transition name="slide">
+        <div
+          v-show="showNavigation"
+          id="is-navigation"
+          class="hero is-fullheight is-primary is-bold">
+        
+          <div class="hero-head">
+            <div class="section">
+              <div class="container has-text-white">
+                <div @click="toggleNavigation" class="is-menu-button has-text-white">
+                  <b-icon icon="close"/> 
+                </div>
+                <h1 class="title is-size-2">dmtool</h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="hero-body">
-          <div class="container">
-            
-            <nuxt-link
-              v-for="route in routes"
-              :key="route.name"
-              :to="route.path"
-              tag="li"
-              class="button is-capitalized">
-              {{ route.name }}
-            </nuxt-link>
 
+          <div class="hero-body">
+            <div class="container">
+            
+              <nuxt-link
+                to="/magic-items"
+                class="button is-medium is-capitalized"
+                @click.native="toggleNavigation">
+                Magic Items
+              </nuxt-link>
+              <nuxt-link
+                to="/spells"
+                class="button is-medium is-capitalized"
+                @click.native="toggleNavigation">
+                Spells
+              </nuxt-link>
+              <nuxt-link
+                to="/bestiary"
+                class="button is-medium is-capitalized"
+                @click.native="toggleNavigation">
+                Bestiary
+              </nuxt-link>
+              <nuxt-link
+                to="/encounter"
+                class="button is-medium is-capitalized"
+                @click.native="toggleNavigation">
+                Encounter
+              </nuxt-link>
+
+            </div>
           </div>
+
         </div>
-      </div>
-    </transition>
+      </transition>
+    </div>
 
     <!-- content -->
     <nuxt/>
@@ -56,60 +84,68 @@ export default {
     }
   },
 
-  computed: {
-    routes: function() {
-      let routes = this.$router.options.routes
-      routes.forEach(
-        route =>
-          route.name === 'magic-items'
-            ? (route.name = 'magic items')
-            : route.name === 'index' ? (route.name = 'home') : route.name
-      )
-      return routes
-    }
-  },
-
   methods: {
     toggleNavigation: function() {
       this.showNavigation = !this.showNavigation
+      // Set document overflow
+      document.documentElement.style.overflowY = this.showNavigation
+        ? 'hidden'
+        : 'scroll'
+      // Set navigation and body overflow; prevents things jumping around on page when overflow changes
+      document.getElementById(
+        'is-navigation'
+      ).style.overflowY = document.body.style.overflowY = this.showNavigation
+        ? 'scroll'
+        : 'auto'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.is-navigation {
-  z-index: 1000000;
-  position: fixed;
-  top: 0;
-  min-width: 30vw;
-  width: 30%;
-  background-color: red;
-  box-shadow: 0 0 75px rgba($color: #000000, $alpha: 0.3);
-  @media screen and (max-width: 768px) {
-    width: 100vw;
+.page-header {
+  #is-navigation {
+    z-index: 1000000;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    box-shadow: 0 0 75px rgba($color: #000000, $alpha: 0.3);
   }
-}
 
-$slide-time: 300ms;
-.slide {
-  &-enter-active,
-  &-leave-active {
-    transition: transform $slide-time ease-in-out;
-  }
-  &-enter,
-  &-leave-to {
-    transform: translateX(-30vw);
-    box-shadow: none;
-    @media screen and (max-width: 768px) {
-      transform: translateX(-100vw);
+  .slide {
+    &-enter-active,
+    &-leave-active {
+      transition: transform 300ms ease-in-out;
+      @media screen and (max-width: 768px) {
+        transition: transform 200ms ease-in-out;
+      }
+    }
+    &-enter,
+    &-leave-to {
+      transform: translateY(-100%);
+      box-shadow: none;
+      @media screen and (max-width: 768px) {
+        transform: translateX(-100%);
+      }
     }
   }
-}
 
-.button.is-text.has-text-white {
-  &:hover {
-    background: none;
+  .is-menu-button {
+    cursor: pointer;
+  }
+
+  .title,
+  .is-menu-button {
+    display: inline-block;
+  }
+
+  h1.title {
+    margin-left: 1rem;
+  }
+
+  .section {
+    padding: 1.5rem;
+    margin-bottom: -3rem;
   }
 }
 </style>
