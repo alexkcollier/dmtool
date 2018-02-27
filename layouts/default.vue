848eb169bd2af64efa14d2be0ff28bdbaecf1b31
Alex Collier <alexkcollier@gmail.com>
2018-02-27 11:53:17 -0500

    Format menu content

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,14 @@
     <div class="page-header">
       <div class="section">
         <div class="container">
-          <div @click="toggleNavigation" class="is-menu-button has-text-primary">
-            <b-icon icon="menu"/>
+          <div @click="toggleNavigation" class="is-menu-button" >
+            <div class="hamburger" :class="{'is-active': showNavigation}">
+              <span/>
+              <span/>
+              <span/>
+            </div>
+            <h1 class="title is-size-2" :class="{'has-text-white': showNavigation}">dmtool</h1>
           </div>
-          <h1 class="title is-size-2">dmtool</h1>
         </div>
       </div>
 
@@ -17,45 +21,34 @@
           id="is-navigation"
           class="hero is-fullheight is-primary is-bold">
         
-          <div class="hero-head">
-            <div class="section">
-              <div class="container has-text-white">
-                <div @click="toggleNavigation" class="is-menu-button has-text-white">
-                  <b-icon icon="close"/> 
-                </div>
-                <h1 class="title is-size-2">dmtool</h1>
-              </div>
-            </div>
-          </div>
-
           <div class="hero-body">
-            <div class="container">
-            
+            <div class="container has-text-centered">
+                
               <nuxt-link
                 to="/magic-items"
-                class="button is-medium is-capitalized"
+                class="button is-text is-medium is-capitalized is-block-mobile has-text-white"
                 @click.native="toggleNavigation">
                 Magic Items
               </nuxt-link>
               <nuxt-link
                 to="/spells"
-                class="button is-medium is-capitalized"
+                class="button is-text is-medium is-capitalized is-block-mobile has-text-white"
                 @click.native="toggleNavigation">
                 Spells
               </nuxt-link>
               <nuxt-link
                 to="/bestiary"
-                class="button is-medium is-capitalized"
+                class="button is-text is-medium is-capitalized is-block-mobile has-text-white"
                 @click.native="toggleNavigation">
                 Bestiary
               </nuxt-link>
               <nuxt-link
                 to="/encounter"
-                class="button is-medium is-capitalized"
+                class="button is-text is-medium is-capitalized is-block-mobile has-text-white"
                 @click.native="toggleNavigation">
                 Encounter
               </nuxt-link>
-
+            
             </div>
           </div>
 
@@ -112,12 +105,15 @@ export default {
     box-shadow: 0 0 75px rgba($color: #000000, $alpha: 0.3);
   }
 
+  $desktop-transition-time: 300ms;
+  $mobile-transition-time: 200ms;
+
   .slide {
     &-enter-active,
     &-leave-active {
-      transition: transform 300ms ease-in-out;
+      transition: transform $desktop-transition-time ease-in-out;
       @media screen and (max-width: 768px) {
-        transition: transform 200ms ease-in-out;
+        transition-duration: $mobile-transition-time;
       }
     }
     &-enter,
@@ -130,22 +126,91 @@ export default {
     }
   }
 
-  .is-menu-button {
-    cursor: pointer;
-  }
-
-  .title,
-  .is-menu-button {
-    display: inline-block;
-  }
-
-  h1.title {
-    margin-left: 1rem;
-  }
-
   .section {
     padding: 1.5rem;
     margin-bottom: -3rem;
+  }
+
+  .is-block-mobile {
+    @media screen and (max-width: 768px) {
+      margin: 1.75rem auto;
+    }
+  }
+
+  .button.is-text.has-text-white:hover {
+    background: none;
+  }
+
+  .hero::before {
+    height: 90%;
+    width: 100%;
+    position: absolute;
+    top: 5vh;
+    background: url('~assets/ampersand.svg') center center / contain no-repeat;
+    background-blend-mode: multiply;
+    mix-blend-mode: multiply;
+    opacity: 0.1;
+    content: '';
+  }
+
+  .is-menu-button {
+    display: inline-block;
+    position: relative;
+    z-index: 10000000;
+    cursor: pointer;
+    transition: color $desktop-transition-time ease-in-out;
+    @media screen and (max-width: 768px) {
+      transition-duration: $mobile-transition-time;
+    }
+    .title {
+      display: inline-block;
+      margin-left: 1rem;
+    }
+    .hamburger {
+      cursor: pointer;
+      display: inline-block;
+      position: relative;
+      height: 1rem;
+      width: 1rem;
+      span {
+        background-color: #3d4f5d;
+        display: block;
+        height: 2px;
+        left: calc(50% - 8px);
+        position: absolute;
+        transform-origin: center;
+        transition-duration: $desktop-transition-time;
+        @media screen and (max-width: 768px) {
+          transition-duration: $mobile-transition-time;
+        }
+        transition-property: background-color, opacity, transform;
+        transition-timing-function: ease-in-out;
+        width: 1rem;
+        &:nth-child(1) {
+          top: calc(50% - 6px);
+        }
+        &:nth-child(2) {
+          top: calc(50% - 1px);
+        }
+        &:nth-child(3) {
+          top: calc(50% + 4px);
+        }
+      }
+      &.is-active {
+        span {
+          background-color: white;
+          &:nth-child(1) {
+            transform: translateY(5px) rotate(45deg);
+          }
+          &:nth-child(2) {
+            opacity: 0;
+          }
+          &:nth-child(3) {
+            transform: translateY(-5px) rotate(-45deg);
+          }
+        }
+      }
+    }
   }
 }
 </style>
