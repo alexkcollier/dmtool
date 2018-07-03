774041179b801a0ef8fbe30b616c779a11108e1b
Alex Collier <alexkcollier@gmail.com>
2018-07-03 13:19:02 -0400

    Refactors

--- store/toggle-active-el.js
+++ store/toggle-active-el.js
@@ -3,27 +3,25 @@ export const state = () => ({
 })
 
 export const mutations = {
-  SET_ACTIVE_EL(state, payload) {
-    state.activeEl = payload
-  }
+  SET_ACTIVE_EL: (state, payload) => (state.activeEl = payload)
 }
 
 export const actions = {
-  SET_ACTIVE_EL({ dispatch, commit, state }, { el, delay }) {
+  SET_ACTIVE_EL: ({ dispatch, commit, state }, { el, delay }) => {
     const oldActive = state.activeEl !== null
-    return dispatch('CLEAR_ACTIVE_EL').then(() => {
-      return new Promise((resolve, reject) =>
-        setTimeout(() => {
-          commit('SET_ACTIVE_EL', el)
-          resolve()
-        }, oldActive ? delay : 0)
-      )
-    })
+    return dispatch('CLEAR_ACTIVE_EL').then(
+      () =>
+        new Promise(resolve =>
+          setTimeout(() => {
+            commit('SET_ACTIVE_EL', el)
+            resolve()
+          }, oldActive ? delay : 0)
+        )
+    )
   },
-  CLEAR_ACTIVE_EL({ commit }) {
-    return new Promise((resolve, reject) => {
-      commit('SET_ACTIVE_EL', null)
-      resolve()
-    })
+
+  CLEAR_ACTIVE_EL: ({ commit }) => {
+    commit('SET_ACTIVE_EL', null)
+    return Promise.resolve()
   }
 }
