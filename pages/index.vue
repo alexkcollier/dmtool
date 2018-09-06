d6e059e6599b4e5be5a4c2397a04d8372d0befaf
alexkcollier <alexkcollier@gmail.com>
2018-09-06 17:13:39 -0400

    removes lottie

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,6 @@
   <section class="hero is-primary is-bold is-fullheight">
     <div class="hero-body">
       
-      <!-- Animation -->
-      <Lottie 
-        :options="options"
-        class="ampersand-animation"/>
-
       <div class="container has-text-centered">
         <h1 class="title is-size-1">
           dmtool
@@ -26,24 +21,8 @@
 </template>
 
 <script>
-import Lottie from 'vue-lottie/src/lottie.vue'
-import * as animationData from '~/assets/animations/ampersand-animation.json'
-
 export default {
   layout: 'HomePage',
-
-  components: {
-    Lottie
-  },
-
-  data() {
-    return {
-      options: {
-        animationData,
-        loop: false
-      }
-    }
-  },
 
   head() {
     return { title: 'Home' }
@@ -52,11 +31,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.ampersand-animation {
-  margin: 0 auto;
-  max-width: 90vw;
-  mix-blend-mode: darken;
-  opacity: 0.2;
-  position: absolute;
+.hero-body {
+  &::before {
+    background: url('~assets/ampersand.svg') center center / contain no-repeat;
+    background-blend-mode: multiply;
+    content: '';
+    height: 50vh;
+    left: 0;
+    mix-blend-mode: multiply;
+    opacity: 0.1;
+    position: absolute;
+    width: 100%;
+  }
 }
 </style>
