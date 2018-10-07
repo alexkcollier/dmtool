bbb44dca8be74dc0e6ec070a32808930f5ebd554
Alex Collier <alexkcollier@gmail.com>
2018-10-06 20:17:11 -0400

    fix: adds remaining entries

--- data/magic-items/index.js
+++ data/magic-items/index.js
@@ -3,103 +3,8 @@ import { excludeTypes } from './exclude-types'
 import { item, itemGroup } from './items.json'
 import { variant } from './magicvariants.json'
 
-// merge all items
-// TODO: should parsedVariants be merged here, then parsed?
-const itemsToParse = [...item, ...itemGroup]
-
-// format items
-const parsedItems = itemsToParse.reduce((acc, item) => {
-  // exclude mundane items
-  if (item.type && excludeTypes.includes(item.type.toLowerCase())) return acc
-
-  // remove extra rarity
-  if (item.rarity === 'Unknown (Magic)') item.rarity = 'Unknown'
-
-  // armor of resistance should be in `./magicvariants.json` , but isn't
-  if (item.name.toLowerCase() === 'armor of resistance') {
-    item.type = 'Armor'
-    item.subtype = 'light, medium, or heavy'
-  }
-
-  // items with no type
-  if (!item.type && item.technology) item.type = item.technology
-  if (!item.type && item.wondrous) item.type = 'Wondrous item'
-
-  // handle weapons (Ammunition, Ranged, Melee)
-  if (['A', 'R', 'M'].includes(item.type)) {
-    item.type = 'Weapon'
-    if (item.baseItem) item.subtype = item.baseItem.split('|').shift()
-  }
-
-  // handle armor
-  if (item.armor) {
-    switch (item.type) {
-      case 'LA':
-        // eslint-disable-next-line eqeqeq
-        if (item.ac === 11 && item.weight == 8) item.subtype = 'Padded'
-        // eslint-disable-next-line eqeqeq
-        if (item.ac === 11 && item.weight == 10) item.subtype = 'Leather'
-        if (item.ac === 12) item.subtype = 'Studded leather'
-        break
-
-      case 'MA':
-        if (item.ac === 12) item.subtype = 'Hide'
-        if (item.ac === 13) item.subtype = 'Chain shirt'
-        // eslint-disable-next-line eqeqeq
-        if (item.ac === 14 && item.weight == 45) item.subtype = 'Scale mail'
-        // eslint-disable-next-line eqeqeq
-        if (item.ac === 14 && item.weight == 20) item.subtype = 'Breastplate'
-        if (item.ac === 15) item.subtype = 'Half plate'
-        break
-
-      case 'HA':
-        if (item.ac === 14) item.subtype = 'Ring mail'
-        if (item.ac === 16) item.subtype = 'Chain mail'
-        if (item.ac === 17) item.subtype = 'Splint'
-        if (item.ac === 18) item.subtype = 'Plate'
-        break
-    }
-
-    item.type = 'Armor'
-  }
-
-  // handle all other items
-  switch (item.type) {
-    case 'EM':
-      item.type = 'Eldritch machine'
-      break
-    case 'OTH':
-      item.type = 'Other'
-      break
-    case 'P':
-      item.type = 'Potion'
-      break
-    case 'RD':
-      item.type = 'Rod'
-      break
-    case 'RG':
-      item.type = 'Ring'
-      break
-    case 'S':
-      item.type = 'Shield'
-      break
-    case 'SC':
-      item.type = 'Scroll'
-      break
-    case 'WD':
-      item.type = 'Wand'
-      break
-
-    default:
-      break
-  }
-
-  acc.push(item)
-  return acc
-}, [])
-
 // format variants
-const parsedVariants = variant
+const transformedVariants = variant
   // remove nonsense items
   .filter(v => !v.name.match(/Weapon \(no damage\)/) && v.name !== 'Armblade')
   .reduce((acc, variant) => {
@@ -226,4 +131,130 @@ const parsedVariants = variant
     return acc
   }, [])
 
-export default [...custom, ...parsedItems, ...parsedVariants]
+// merge all items
+const itemsToParse = [...item, ...itemGroup, ...transformedVariants]
+
+// format items
+const parsedItems = itemsToParse.reduce((acc, item) => {
+  // exclude mundane items
+  if (item.type && excludeTypes.includes(item.type.toLowerCase())) return acc
+
+  // remove extra rarity
+  if (item.rarity === 'Unknown (Magic)') item.rarity = 'Unknown'
+
+  // armor of resistance should be in `./magicvariants.json` , but isn't
+  if (item.name.toLowerCase() === 'armor of resistance') {
+    item.type = 'Armor'
+    item.subtype = 'light, medium, or heavy'
+  }
+
+  // items with no type
+  if (!item.type && item.technology) item.type = item.technology
+  if (!item.type && item.wondrous) item.type = 'Wondrous item'
+
+  // handle weapons (Ammunition, Ranged, Melee)
+  if (['A', 'R', 'M'].includes(item.type)) {
+    item.type = 'Weapon'
+    if (item.baseItem) item.subtype = item.baseItem.split('|').shift()
+  }
+
+  // handle armor
+  if (item.armor) {
+    switch (item.type) {
+      case 'LA':
+        // eslint-disable-next-line eqeqeq
+        if (item.ac === 11 && item.weight == 8) item.subtype = 'Padded'
+        // eslint-disable-next-line eqeqeq
+        if (item.ac === 11 && item.weight == 10) item.subtype = 'Leather'
+        if (item.ac === 12) item.subtype = 'Studded leather'
+        break
+
+      case 'MA':
+        if (item.ac === 12) item.subtype = 'Hide'
+        if (item.ac === 13) item.subtype = 'Chain shirt'
+        // eslint-disable-next-line eqeqeq
+        if (item.ac === 14 && item.weight == 45) item.subtype = 'Scale mail'
+        // eslint-disable-next-line eqeqeq
+        if (item.ac === 14 && item.weight == 20) item.subtype = 'Breastplate'
+        if (item.ac === 15) item.subtype = 'Half plate'
+        break
+
+      case 'HA':
+        if (item.ac === 14) item.subtype = 'Ring mail'
+        if (item.ac === 16) item.subtype = 'Chain mail'
+        if (item.ac === 17) item.subtype = 'Splint'
+        if (item.ac === 18) item.subtype = 'Plate'
+        break
+    }
+
+    item.type = 'Armor'
+  }
+
+  // handle all other items
+  switch (item.type) {
+    case 'EM':
+      item.type = 'Eldritch machine'
+      break
+    case 'OTH':
+      item.type = 'Other'
+      break
+    case 'P':
+      item.type = 'Potion'
+      break
+    case 'RD':
+      item.type = 'Rod'
+      break
+    case 'RG':
+      item.type = 'Ring'
+      break
+    case 'S':
+      item.type = 'Shield'
+      break
+    case 'SC':
+      item.type = 'Scroll'
+      break
+    case 'WD':
+      item.type = 'Wand'
+      break
+
+    default:
+      break
+  }
+
+  // Add entries to items without entries
+  if (!item.entries) item.entries = []
+
+  if (item.type === 'Armor') {
+    if (item.resist) {
+      item.entries.push(`You have resistance to ${item.resist} damage while you
+      wear this armor.`)
+    }
+
+    if (item.stealth) {
+      item.entries.push(`The wearer has disadvantage on Stealth (Dexterity)
+      checks.`)
+    }
+
+    const heavyArmors = ['Ring mail', 'Chain mail', 'Splint', 'Plate']
+
+    if (heavyArmors.includes(item.subType) && item.strength) {
+      item.entries.push(`If the wearer has a Strength score lower than
+      ${item.strength}, their speed is reduced by 10 feet.`)
+    }
+  } else if (item.resist) {
+    if (item.type === 'Potion') {
+      item.entries.push(`When you drink this potion, you gain resistance to
+      ${item.resist} damage for 1 hour.`)
+    }
+
+    if (item.type === 'Ring') {
+      item.entries.push(`You have resistance to ${item.resist} damage while
+      wearing this ring.`)
+    }
+  }
+
+  acc.push(item)
+  return acc
+}, [])
+
+export default [...custom, ...parsedItems]
