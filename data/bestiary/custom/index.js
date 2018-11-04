a356baf09d187ec781a3cdab47df9b5e64ac1d9c
Alex Collier <alexkcollier@gmail.com>
2018-11-03 22:28:08 -0400

    feat(bestiary): adds dluur

--- data/bestiary/custom/index.js
+++ data/bestiary/custom/index.js
@@ -1,7 +1,8 @@
 import Durgeddin from './durgeddin.json'
 import Norixius from './norixius.json'
 import PaleOrcs from './PaleOrcs.json'
+import BookOfBeautifulHorrors from './bestiary-bbh.json'
 
 export default {
-  monster: [Durgeddin, Norixius, ...PaleOrcs]
+  monster: [Durgeddin, Norixius, ...PaleOrcs, ...BookOfBeautifulHorrors]
 }
