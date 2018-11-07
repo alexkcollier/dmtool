a9b435ad596f7f26c09a0577a9633262eff089f2
Alex Collier <>
2018-11-07 11:11:12 -0500

    feat: adds new regexps

--- plugins/entry-helper.js
+++ plugins/entry-helper.js
@@ -5,13 +5,66 @@ const italicReplace = '<em>$1</em>'
 const plainReplace = '$1'
 
 export const regExps = {
+  bold: {
+    find: /{@(bold|b)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: '<strong>$2</strong>'
+  },
+  chance: {
+    find: /{@chance\s(.*?)(\|(.*?))?(\|(.*?))?}/g,
+    html: '$1 percent'
+  },
+  creature: {
+    find: /{@creature\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: boldReplace
+  },
+  damage: {
+    find: /{@damage\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: plainReplace
+  },
+  dice: {
+    find: /{@dice\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: plainReplace
+  },
+  diceExpression: {
+    // (XdY+/-Z) => (XdY +/- Z)
+    find: /(\(\d+d\d{1,2})([+-])(\d+\))/g,
+    html: '$1&nbsp;$2&nbsp;$3'
+  },
+  hazard: {
+    find: /{@hazard\s(.*?)(\|(.*?))?(\|(.*?))?}/g,
+    html: plainReplace
+  },
+  hit: {
+    find: /{@hit\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: plainReplace
+  },
+  hitLabel: {
+    find: /(Hit:|Miss:)/,
+    html: italicReplace
+  },
+  hitTag: {
+    find: /{@h}/g,
+    html: '<em>Hit: </em>'
+  },
+  hitUnsigned: {
+    find: /{@hit\s(\d*?)(\|(.*?))?(\|.*?)?}/g,
+    html: `+${plainReplace}`
+  },
+  italic: {
+    find: /{@(italic|i)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: '<em>$2</em>'
+  },
+  item: {
+    find: /{@item\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: '<a href="/magic-items?name=$1" class="link">$1</a>'
+  },
+  label: {
+    find: /^((\w+\s*){0,3}:)/g,
+    html: italicReplace
+  },
   meleeWeaponAttack: {
     find: /{@atk\s(mw?)(\|(.*?))?(\|.*?)?}/g,
     html: '<em>Melee Weapon Attack: </em>'
-  },
-  rangedWeaponAttack: {
-    find: /{@atk\s(rw?)(\|(.*?))?(\|.*?)?}/g,
-    html: '<em>Ranged Weapon Attack: </em>'
   },
   meleeOrRangedWeaponAttack: {
     find: /{@atk\s(mw,rw?)(\|(.*?))?(\|.*?)?}/g,
@@ -25,62 +78,29 @@ export const regExps = {
     find: /{@atk\s(rs?)(\|(.*?))?(\|.*?)?}/g,
     html: '<em>Ranged Spell Attack: </em>'
   },
+  rangedWeaponAttack: {
+    find: /{@atk\s(rw?)(\|(.*?))?(\|.*?)?}/g,
+    html: '<em>Ranged Weapon Attack: </em>'
+  },
   meleeOrRangedSpellAttack: {
     find: /{@atk\s(ms,rs?)(\|(.*?))?(\|.*?)?}/g,
     html: '<em>Melee or Ranged Spell Attack: </em>'
   },
-  creature: {
-    find: /{@creature\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: boldReplace
+  other: {
+    find: /{@(filter|condition|race)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: '$2'
   },
-  dice: {
-    find: /{@dice\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: plainReplace
-  },
-  diceExpression: {
-    // (XdY+/-Z) => (XdY +/- Z)
-    find: /(\(\d+d\d{1,2})([+-])(\d+\))/g,
-    html: '$1&nbsp;$2&nbsp;$3'
-  },
-  noSignHit: {
-    find: /{@hit\s(\d*?)(\|(.*?))?(\|.*?)?}/g,
-    html: `+${plainReplace}`
-  },
-  hitLabel: {
-    find: /(Hit:)/,
-    html: italicReplace
-  },
-  hit: {
-    find: /{@hit\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: plainReplace
-  },
-  spell: {
-    find: /{@spell\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: italicReplace
-  },
-  item: {
-    find: /{@item\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: '<a href="/magic-items?name=$1" class="link">$1</a>'
-  },
-  label: {
-    find: /^((\w+\s*){0,3}:)/g,
-    html: italicReplace
-  },
-  bold: {
-    find: /{@(bold|b)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: '<strong>$2</strong>'
-  },
-  italic: {
-    find: /{@(italic|i)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: '<em>$2</em>'
+  scaleDice: {
+    find: /{@scaledice\s(.*?)(\|(.*?))?(\|(.*?))?}/g,
+    html: '$5'
   },
   skill: {
     find: /{@skill\s(.*?)(\|(.*?))?(\|.*?)?}/g,
     html: plainReplace
   },
-  other: {
-    find: /{@(filter|condition|race)\s(.*?)(\|(.*?))?(\|.*?)?}/g,
-    html: '$2'
+  spell: {
+    find: /{@spell\s(.*?)(\|(.*?))?(\|.*?)?}/g,
+    html: italicReplace
   }
 }
 
