47d9e693e68d5a909f629709446ae5f78f2656c9
alexkcollier <alexkcollier@gmail.com>
2018-11-11 22:04:04 -0500

    feat(bestiary): adds banshee queen

--- data/bestiary/custom/index.js
+++ data/bestiary/custom/index.js
@@ -1,8 +1,9 @@
+import BansheeQueen from './banshee-queen.json'
+import BookOfBeautifulHorrors from './bestiary-bbh.json'
 import Durgeddin from './durgeddin.json'
 import Norixius from './norixius.json'
 import PaleOrcs from './PaleOrcs.json'
-import BookOfBeautifulHorrors from './bestiary-bbh.json'
 
 export default {
-  monster: [Durgeddin, Norixius, ...PaleOrcs, ...BookOfBeautifulHorrors]
+  monster: [Durgeddin, Norixius, ...PaleOrcs, ...BookOfBeautifulHorrors, BansheeQueen]
 }
