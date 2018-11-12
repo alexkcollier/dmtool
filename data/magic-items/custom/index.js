8a9fc99b7c75f176858ec2f7c340cce19d4dbbca
Alex Collier <>
2018-11-12 13:22:08 -0500

    feat(magic-items): adds endless walk set
    - Full set and Compass Rose are hidden

--- data/magic-items/custom/index.js
+++ data/magic-items/custom/index.js
@@ -1,4 +1,9 @@
 import CrownOfKhundrukar from './crown-of-khundrukar.json'
+import EndlessWalk from './endless-walk.json'
 import FangOfZilvinas from './fang-of-zilvinas.json'
 
-export default [CrownOfKhundrukar, FangOfZilvinas]
+// TODO: unhide
+// Hide the remaining set items for now
+const TravelersPledge = EndlessWalk.find(item => item.name === "Traveler's Pledge")
+
+export default [CrownOfKhundrukar, TravelersPledge, FangOfZilvinas]
