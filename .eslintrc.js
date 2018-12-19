34bc7cafa97b454af27102b185db75e22a556f82
Alex Collier <>
2018-12-19 11:10:26 -0500

    chore: uses shareable eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,6 @@
 module.exports = {
-  root: true,
   parserOptions: {
     parser: 'babel-eslint'
   },
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: ['standard', 'plugin:prettier/recommended', 'prettier', 'plugin:vue/recommended'],
-  plugins: ['standard', 'vue', 'prettier'],
-  rules: {
-    semi: [2, 'never'],
-    'no-console': 'off',
-    'prettier/prettier': ['error'],
-    'vue/no-v-html': 'off',
-    'vue/component-name-in-template-casing': 'off'
-  }
+  extends: ['@acollier/eslint-config-vue']
 }
