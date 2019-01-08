abe86cc4699e1bdcce56ad78ff1bd4b0713f0e34
alexkcollier <alexkcollier@gmail.com>
2019-01-08 15:01:50 -0500

    fix(encounter): fixes creatures not being removed on navigation/refresh

--- store/encounter.js
+++ store/encounter.js
@@ -10,11 +10,16 @@ export const getters = {
 
 export const mutations = {
   ADD_TO_ENCOUNTER: (state, payload) => {
-    if (!state.encounter.includes(payload)) state.encounter.push(payload)
+    if (!state.encounter.map(c => c.name + c.source).includes(payload.name + payload.source)) {
+      state.encounter.push(payload)
+    }
   },
 
-  REMOVE_FROM_ENCOUNTER: (state, payload) =>
-    (state.encounter = state.encounter.filter(e => e !== payload)),
+  REMOVE_FROM_ENCOUNTER: (state, payload) => {
+    state.encounter = state.encounter.filter(c => {
+      return c.name !== payload.name && c.source !== payload.source
+    })
+  },
 
   CLEAR_ENCOUNTER: state => (state.encounter = [])
 }
