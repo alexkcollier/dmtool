3365e01f3403c923aa62b1cbe0e82fd967107bed
alexkcollier <alexkcollier@gmail.com>
2019-01-11 11:06:18 -0500

    perf: PWA improvements

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,20 @@
 import dotenv from 'dotenv'
 import { routes } from './routes'
+import pkg from './package.json'
 dotenv.config()
 
+const brandColor = '#3d4f5d'
+const name = pkg.name
 export default {
   mode: 'spa',
+  modern: 'client',
   head: {
     titleTemplate: t => (t ? `${t} - dmtool` : 'dmtool'),
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'D&D 5e Utilities' },
-      { name: 'theme-color', content: '#3d4f5d' }
+      { name: 'theme-color', content: brandColor }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -20,8 +24,21 @@ export default {
         href: '//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css'
       },
       {
-        rel: 'prefetch',
-        href: '//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css'
+        rel: 'preload',
+        href: '//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css',
+        crossorigin: 'anonymous',
+        as: 'style'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://use.typekit.net/fnw1iwa.css'
+      },
+      {
+        rel: 'preload',
+        href: 'https://use.typekit.net/fnw1iwa.css',
+        crossorigin: 'anonymous',
+        as: 'style'
       }
     ]
   },
@@ -34,9 +51,9 @@ export default {
   modules: ['@nuxtjs/pwa'],
   manifest: {
     name: 'DM Tool',
-    short_name: 'dmtool',
-    background_color: '#3d4f5d',
-    theme_color: '#3d4f5d'
+    short_name: name,
+    background_color: brandColor,
+    theme_color: brandColor
   },
   workbox: {
     runtimeCaching: [
@@ -45,7 +62,8 @@ export default {
         handler: 'cacheFirst'
       },
       {
-        urlPattern: '.*//*.typekit.net/.*'
+        urlPattern: '.*//*.typekit.net/.*',
+        handler: 'cacheFirst'
       }
     ]
   },
@@ -71,6 +89,8 @@ export default {
         }
       }
     },
+    optimization: { splitChunks: { maxSize: 244000 } },
+    extractCSS: true,
     extend(config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.devtool = '#eval-source-map'
